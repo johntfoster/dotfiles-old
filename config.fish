12fbeaa2254eaaf9b6f45c483d9220f2ff7ab5c2
John Foster <johntfosterjr@gmail.com>
2015-09-29 09:15:03 -0500

    Remove Github token from config.fish
    It's been revoked so no need to clean up commit histories.

--- config.fish
+++ config.fish
@@ -9,5 +9,3 @@ source ~/.tacklebox/tacklebox.fish
 set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
 powerline-setup
 
-
-set -x HOMEBREW_GITHUB_API_TOKEN 4a856b15b6245d9b7b996e39eb547d583167b0c0
