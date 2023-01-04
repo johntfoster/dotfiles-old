3626012f6d081c3b17278cdcacf8a2b5291e27ff
John T. Foster <johntfosterjr@gmail.com>
2023-01-04 13:09:17 -0600

    Remove unneeded env variables

--- install.sh
+++ install.sh
@@ -22,7 +22,6 @@ echo "Initializing conda for fish."
 conda init fish
 conda install mamba -c conda-forge -y
 $CONDA_DIR/bin/pip install powerline-status
-export CONDA_BASE_SITE_PACKAGES=`$CONDA_DIR/bin/python -c 'import site; print(site.getsitepackages()[0])'`
 
 echo "Installing fonts."
 FONT_DIR="$HOME/.fonts"
