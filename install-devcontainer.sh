5a742f404bb7c325d94432a39eafaae6bef285f9
John T. Foster <johntfosterjr@gmail.com>
2023-01-06 07:19:07 -0600

    Add specialized install for devcontainers

---
+++ install-devcontainer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    rm -rf ~/.bashrc
+    mkdir -p $HOME/.config/fish
+    ln -s $script_dir/bash_profile ~/.bash_profile
+    ln -s $script_dir/bashrc ~/.bashrc
+    ln -s $script_dir/tmux.conf ~/.tmux.conf
+    ln -s $script_dir/config.fish ~/.config/fish/config.fish
+}
+
+create_symlinks
+
+sudo apt-get update && export DEBIAN_FRONTEND=noninteractive \
+     && apt-get -y install --no-install-recommends fish tmux
+
+echo "Initializing conda for fish."
+conda init fish
+$CONDA_DIR/bin/pip install powerline-status
+
+echo "Installing fonts."
+FONT_DIR="$HOME/.fonts"
+git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
+cd $FONT_DIR
+./install.sh
+
+#Install OMF
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+echo "omf install bobthefish" | fish
+echo "omf theme bobthefish" | fish
